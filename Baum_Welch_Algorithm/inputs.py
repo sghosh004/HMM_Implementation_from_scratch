fc019f3727d4f4b085ef07bce886b817be0a1fcd
sghosh004 <ibpdsp2004@gmail.com>
2025-02-11 17:52:43 +0530

    Create inputs.py

---
+++ Baum_Welch_Algorithm/inputs.py
@@ -0,0 +1,11 @@
+transition_matrix=[[0.3, 0.7],
+                   [0.4, 0.6]]
+
+emission_matrix=[[0.8, 0.2],
+                 [0.3, 0.7]]
+
+initial_probability=[0.2, 0.8]
+states=['s', 't']
+states_dic={'s': 0, 't': 1}
+sequence_syms={"A":0, "B": 1}
+sequence='ABBA'                
